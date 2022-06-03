2fd33b430a25c6fe530e2faa8e85f1623fed45e1
max wu <wutiger555@gmail.com>
2022-06-04 03:00:04 +0800

    bug fix

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
 require("dotenv").config();
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const path = require("path");
 
-app.use(cors())
+app.use(cors());
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const joblistRouter = require('./routes/joblist')
-app.use('/job', joblistRouter)
+const joblistRouter = require("./routes/joblist");
+app.use("/job", joblistRouter);
 
-const illegalRouter = require('./routes/illegal')
-app.use('/illegal', illegalRouter)
+const illegalRouter = require("./routes/illegal");
+app.use("/illegal", illegalRouter);
 
-const capitalRouter = require('./routes/capital')
-app.use('/capital', capitalRouter)
+const capitalRouter = require("./routes/capital");
+app.use("/capital", capitalRouter);
 
 if (process.env.NODE_ENV === "production") {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, "client/build")));
-    // Handle React routing, return all requests to React app
-    app.get("*", function (req, res) {
-      res.sendFile(path.join(__dirname, "client/build", "index.html"));
-    });
-  }
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
