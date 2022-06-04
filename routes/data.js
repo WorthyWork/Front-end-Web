8b915d4ccc49235c5f9d010a5cc68b95d1cdbf03
max wu <wutiger555@gmail.com>
2022-06-04 19:13:01 +0800

    MODIFY: heroku config setting fix

--- routes/data.js
+++ routes/data.js
@@ -10,7 +10,7 @@ if (process.env.private_key_id) {
       type: process.env.type,
       project_id: process.env.project_id,
       private_key_id: process.env.private_key_id,
-      private_key: process.env.private_key,
+      private_key: process.env.private_key.replace(/\\n/g, '\n'),
       client_email: process.env.client_email,
       client_id: process.env.client_id,
       auth_url: process.env.auth_url,
